00df52c4f1e397da65c658e12c48d152ef415856
JayashreeGitHub <42576739+JayashreeGitHub@users.noreply.github.com>
2018-08-21 09:48:28 -0400

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+/*
+The Helloworld app implements 
+an application that
+prints Hello World to the standard 
+output
+*/
+
+class HelloWorldApp {
+public static void main(String[] args) {
+System.out.println("Hello World");
+}
